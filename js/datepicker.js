69cae78a798fcd9b526d7f781d7803e3372da55a
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2016-02-14 21:45:41 +0530

    Fix validation bug
    Change date inputs to datepickers.

---
+++ js/datepicker.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('.datepicker').datepicker({
+		dateFormat: "dd-mm-yy",
+		changeMonth: true,
+		changeYear: true,
+		maxDate: 0
+	});
+})
--- js/validate.js
+++ js/validate.js
@@ -1,38 +1,58 @@
-$(document).ready(function(){
-		$('#admForm').validate({
-		rules:{
-			 fName:{
-			 	required:true,
-			    minlength:3,
-			    maxlength:20  }
-			,lName:{
-			 	required:true,
-			    minlength:3,
-			    maxlength:20  }
-			,religion:'required'
-			,dob:'required'
-			,mobNo:{
-				required:true
-				,minlength:10    
-				,maxlength:10    }
-		}
-		messages:{
-			fName:{
-				required:'Enter First Name'
-				,minlength:'Name too short'
-				,maxlength:'Name too long'  }
-
-			,lName:{
-				required:'Enter First Name'
-				,minlength:'Name too short'
-				,maxlength:'Name too long'  }	
-
-			,religion:'Enter your Religion'			
-			,dob:'Enter your BirthDate'
-			,mobNo:{
-				required:'Enter your contact number'
-				,minlength:'Mobile number has to be 10 digits only' 
-				,maxlength:'Mobile number has to be 10 digits only' }
-		}
-		});
+$(function() {
+    $.validator.setDefaults({
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
     });
+
+    $('#admForm').validate({
+        rules: {
+            fName: {
+                required: true,
+                minlength: 3,
+                maxlength: 20
+            },
+            lName: {
+                required: true,
+                minlength: 3,
+                maxlength: 20
+            },
+            pincode: {
+                required: true,
+                minlength: 6,
+                maxlength: 6
+            },
+            mobNo: {
+                required: true,
+                minlength: 10,
+                maxlength: 10
+            }
+        },
+        messages: {
+            fName: {
+                minlength: 'First name must be at least {0} characters long',
+                maxlength: 'First name must be at most {0} characters long'
+            },
+            lName: {
+                minlength: 'Last name must be at least {0} characters long',
+                maxlength: 'Last name must be at most {0} characters long'
+            },
+            mobNo: {
+                minlength: 'Mobile number has to be {0} digits only',
+                maxlength: 'Mobile number has to be {0} digits only'
+            }
+        }
+    });
+});
