8a2e9a893c465225f39a90117ca8bcb47dde773d
ankitk20 <kesharwaniankit80@gmail.com>
2016-02-19 12:17:41 +0530

    put all the tick/cross next to ip boxes

--- js/regexValidation.js
+++ js/regexValidation.js
@@ -1,30 +1,40 @@
 function emailCheck(){ 
-  var regex = /^[^~`!^=@#%$&*+-]{3,}@(g|hot)mail[.]com$/g;
-  var email = document.getElementById('email').value;
-  regex.lastIndex=0;
+  var regex = /^[^~`!^=@#%$&*+-]{3,}@(g|hot)mail[.]com$/;
+  var email = document.getElementById(document.activeElement.id).value;
   var emailTest = regex.test(email);
   if(emailTest){
-    document.getElementById('emailLabel').innerHTML="Validated";
+    document.getElementById(document.activeElement.id).nextSibling.innerHTML="<span class='glyphicon glyphicon-ok'></span>";
+    document.getElementById(document.activeElement.id).nextSibling.style.color="green";
     return true;
   }
-  else{
-    document.getElementById('emailLabel').innerHTML="InvalidEmail";
-    document.getElementById('emailLabel').style.color="red";
+  else if(email!=""){
+    document.getElementById(document.activeElement.id).nextSibling.innerHTML="<span class='glyphicon glyphicon-remove'></span>";
+    document.getElementById(document.activeElement.id).nextSibling.style.color="red";
     return false;
-  }         
+  }
+  else{
+      document.getElementById(document.activeElement.id).nextSibling.innerHTML="";
+      return false;
+  }  
 }
 
 function nameCheck(){
-  var regex = /^$|^[a-zA-Z]{3,20}$/;
+  var regex = /^[a-zA-Z]{3,20}$/;
   var nameId = document.activeElement;
   var name = document.getElementById(nameId.id).value;
   var nameTest = regex.test(name);
   if(nameTest){
-    document.getElementById(nameId.id).nextSibling.innerHTML="";
+    document.getElementById(document.activeElement.id).nextSibling.innerHTML="<span class='glyphicon glyphicon-ok'></span>";
+    document.getElementById(document.activeElement.id).nextSibling.style.color="green";
     return true;
   }
-  else{
-    document.getElementById(nameId.id).nextSibling.innerHTML="Name Mispelled";
+  else if(name!=""){
+    document.getElementById(document.activeElement.id).nextSibling.innerHTML="<span class='glyphicon glyphicon-remove'></span>";
+    document.getElementById(document.activeElement.id).nextSibling.style.color="red";
     return false;
+  }
+  else{
+      document.getElementById(document.activeElement.id).nextSibling.innerHTML="";
+      return false;
   }
 }
